0673818683420820f7fb3ff44ea9dcff0c98988d
Antoine BOURIN <antoine.bourin@outlook.com>
2018-11-17 14:26:28 +0100

    Reformat code & init youtube calls

---
+++ src/app/Services/youtube-channel.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {apiKey} from '../../environments/environment';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeChannelService {
+
+  constructor(private http: HttpClient) { }
+  public getLastVideos(username: string, maxResults: number): Observable {
+    return this.http.get('https://www.googleapis.com/youtube/v3/channels',
+      {
+        params: {
+          'key': apiKey,
+          'forUsername': username,
+          'part': 'snippet,contentDetails,statistics',
+          'maxResults': maxResults.toString(),
+        }
+      });
+  }
+}
--- src/app/heroes-list.ts
+++
@@ -1,7 +0,0 @@
-import {Hero} from './hero';
-
-export const HEROES: Hero[] = [
-    { id: 1, name: 'Jean', power: 600 },
-    { id: 165, name: 'Frédéric', power: 250 },
-    { id: 317, name: 'Youness', power: 120 }
-];
